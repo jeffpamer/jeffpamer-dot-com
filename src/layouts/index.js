949ca50d82012720ed4222a4c6aa12389e8692d0
Jeff Pamer <jeff@mediumdivision.com>
2017-12-17 18:46:07 -0800

    V1 Basic Layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,64 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "tachyons/css/tachyons.min.css";
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
+  <header className="bt bw3 b--navy black-80 pv5">
+    <div className="ph3 ph5-ns">
+      <div className="cf w-100 center mw9">
+        <div
+          className="f1 fw6 ttu tracked f-subheadline-l lh-copy lh-title-l"
+          style={{ fontFamily: "Tungsten A, Tungsten B" }}
         >
-          Gatsby
-        </Link>
-      </h1>
+          Jeff Pamer
+        </div>
+        <div className="f4 lh-copy measure">
+          An experienced full-stack software developer with a love for UX/UI in
+          an unending pursuit of becoming a better craftsman and human.
+        </div>
+        <p className="mb0">jeff@mediumdivision.com</p>
+        <p className="mt0">434-386-7427</p>
+      </div>
     </div>
-  </div>
-)
+  </header>
+);
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="w-100 sans-serif black-80 border-box bg-white">
     <Helmet
-      title="Gatsby Default Starter"
+      title="Jeff Pamer"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {
+          name: "description",
+          content:
+            "Jeff Pamer is a creative full-stack software developer with a love for User Interfaces and a champion for the value of aesthetics."
+        },
+        {
+          name: "keywords",
+          content:
+            "pamer, los angeles, software developer, resume, react, javascript, threejs"
+        }
+      ]}
+      link={[
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "//cloud.typography.com/6718032/7737392/css/fonts.css"
+        }
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
